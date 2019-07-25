4e068cba4780dc2c891db9943444e38a0fdb6338
ietar <410473517@qq.com>
2019-07-25 16:34:07 +0800

    remove the very first version
    and rename

--- draw_cards_arknight.py
+++ draw_cards_arknight.py
@@ -1,16 +1,104 @@
 # author:ietar
-# contact me at 410473517@qq.com
-# 明日方舟模拟抽卡器 调用draw()抽1次 draw(10)则为10次 10次触发保底
+# email: 410473517@qq.com
+# 明日方舟模拟抽卡器 调用draw()抽1次 draw10()则为10次 10次触发保底
 # 也可以draw()其他次数 但只有10次时触发保底
 # 很明显 还有其他功能正在写
-# 写类时稍微检查了输入 抽卡模拟没怎么照顾这个=.=
-# 该寻访为双up 能天使 推进之王各25% 我在想peach?
+# v2.0 模块化 加入了标准池子STANDARD_POOL(无up, 包含限定干员) 自定义up池 池子检查等功能
+# author: ietar
+
 import random
 
-typedict = {0:'先锋', 1:'近卫', 2:'狙击', 3:'医疗', 4:'术士',\
-            5:'特种', 6:'重装', 7:'辅助'}
-RATE = {6:0.02, 5:0.08, 4:0.4, 3:0.5, 2:0, 1:0}
-pool = []
+Official6 = ['推进之王', '能天使', '星熊', '闪灵', '伊芙利特', '银灰', '塞雷娅', '夜莺', '艾雅法拉', '陈', '安洁莉娜', '斯卡蒂']
+Official5 = ['白面鸮', '幽灵鲨', '芙兰卡', '德克萨斯', '凛冬', '普罗旺斯', '蓝毒', '雷蛇', '临光', '红', '赫默', '夜魔', '天火', '初雪', '拉普兰德', '华法琳', '守林人', '狮蝎', '真理', '白金', '陨星', '梅尔', '可颂', '崖心', '空', '食铁兽', '诗怀雅']
+Official4 = ['杜宾', '深海色', '白雪', '远山', '夜烟', '流星', '蛇屠箱', '末药', '猎蜂', '慕斯', '砾', '暗索', '地灵', '调香师', '霜叶', '清道夫', '角峰', '古米', '缠丸', '阿消', '红豆', '杰西卡']
+Official3 = ['芬', '克洛丝', '炎熔', '米格鲁', '芙蓉', '卡缇', '史都华德', '香草', '玫兰莎', '安赛尔', '梓兰', '翎羽', '空爆', '月见夜']
+
+# 干员数据来自http://wiki.joyme.com/arknights/干员寻访模拟器
+# 5星也太多了 我没搞错吧
+
+
+def makepool():
+
+    pool = [] 
+    typedict = {0:'先锋', 1:'近卫', 2:'狙击', 3:'医疗', 4:'术士',5:'特种', 6:'重装', 7:'辅助'}
+
+
+    def addo(**kw):
+        '''add Official'''
+        pool.append(Official(**kw))
+
+        
+    for i in Official6:
+        addo(name=i, stars=6)
+    for i in Official5:
+        addo(name=i, stars=5)
+    for i in Official4:
+        addo(name=i, stars=4)
+    for i in Official3:
+        addo(name=i, stars=3)
+
+
+    return pool
+
+
+def checkpool(pool):
+    '''检查池子内各星级up率是否溢出'''
+    current = {3:0, 4:0, 5:0, 6:0}
+    stars = 0
+    for i in pool:
+        if i.stars == 6:
+            current[6] += i.uprate
+        if i.stars == 5:
+            current[5] += i.uprate
+        if i.stars == 4:
+            current[4] += i.uprate
+        if i.stars == 3:
+            current[3] += i.uprate
+            
+    for i in current:
+        if current[i] > 1:
+            return 0
+        
+    return 1
+    
+
+    
+def set_uprate(pool, name, uprate):
+    '''自定义池子 修改干员up率 重复调用可修改多个 注意每个星级up率之和不能超过1'''
+
+    if uprate > 1 or uprate < 0:
+        raise ValueError('up率必须在0-1之间!')
+    current = {3:0, 4:0, 5:0, 6:0}
+    stars = 0
+    flag = 0
+    for i in pool:
+        if i.name == name:
+            stars = i.stars
+            temp = i
+            flag = 1
+            break;
+
+    if not flag:
+        raise ValueError('池子中找不到该干员,无法修改up率!')
+        
+    assert stars != 0
+    
+    for i in pool:
+        if i.stars == 6:
+            current[6] += i.uprate
+        if i.stars == 5:
+            current[5] += i.uprate
+        if i.stars == 4:
+            current[4] += i.uprate
+        if i.stars == 3:
+            current[3] += i.uprate
+            
+    after_update = uprate - temp.uprate + current[stars]
+    if  after_update > 1:
+        raise ValueError('up率设置过高, 当前{}星up率之和为{}'.format(stars, current[stars]))
+
+    temp.uprate = uprate
+            
 
 
 class Official(object):
@@ -66,10 +154,10 @@ Official((str)name, (int)stars, uprate=0[, (int)otype])'''
 
     @uprate.setter
     def uprate(self, value):
-        if not isinstance(value, float):
-            raise TypeError("uprate should be float!")
-        elif value >= 1 or value <= 0:
-            raise ValueError("uprate should be in [0, 1].")
+        if not isinstance(value, float) and not isinstance(value, int):
+            raise TypeError("uprate should be 0 or float!")
+        elif value > 1 or value < 0:
+            raise ValueError("uprate should be in (0, 1).")
         self._uprate = value
 
     def info(self):
@@ -82,23 +170,31 @@ Official((str)name, (int)stars, uprate=0[, (int)otype])'''
     __repr__ = __str__
 
 
-def addo(*args):
-    pool.append(Official(*args))
-    
-def draw(times=1):
-    
+
+STADNARD_POOL = makepool()
+
+
+def draw10(pool=STADNARD_POOL):
+    return draw(10, pool)
+
+
+def draw(times=1, pool=STADNARD_POOL):
     pool6 = {}
     pool5 = {}
     pool4 = {}
     pool3 = {}
+    RATE = {6:0.02, 5:0.08, 4:0.4, 3:0.5, 2:0, 1:0}
     count6, count5, count4, count3 = 0, 0, 0, 0
     # 分别对应6, 5, 4, 3星 干员出货数量
     ratemax6, ratemax5, ratemax4, ratemax3 = 1, 1, 1, 1
     nonup6, nonup5, nonup4, nonup3 = 0, 0, 0, 0
     # 分别对应6, 5, 4, 3星 非up干员数量
     res = []
+    # pool6,5,4,3 = {干员名:同星级内抽取率}
     #初始化完成
-    
+
+    # if干员的uprate不为0 将其直接设为权重
+    # else 使其平分剩下的rate 分的人数+1
     for i in pool:
         if i.stars == 6:
             pool6[i] = i.uprate
@@ -124,11 +220,7 @@ def draw(times=1):
                 ratemax3 -= i.uprate
             else:
                 nonup3 += 1
-    # 将干员按星级加入对应池子 完成
     
-    # if干员的uprate不为0 将其直接设为权重
-    # else 使其平分剩下的rate 分的人数+1
-
     # nweight == normal weight
     nweight6 = ratemax6/nonup6
     nweight5 = ratemax5/nonup5
@@ -147,11 +239,7 @@ def draw(times=1):
     for i in pool3:
         if not i.uprate:
             pool3[i] = nweight3
-    # 这里uprate的处理还有问题 加起来不等于1 待处理
-    # 处理完成
     
-    # test
-    # print(pool6,pool5,pool4,pool3,sep='\n')
     for _ in range(times):
         a = random.random()
         if a <= RATE[6]:
@@ -175,66 +263,27 @@ def draw(times=1):
     
     # 保底考虑直接重抽10次 只在10连抽时触发 
     if times==10 and not (count6 + count5):
-        print("保底机制使您免收紫气东来困扰 1次")
-        return draw(10)
+        # print("保底机制使您免收紫气东来困扰 1次")
+        return draw(10, pool)
     return res
 
-'''addo('推进之王',6,0.25,'先锋')
-addo('能天使',6,0.25,'狙击')
-addo('星熊',6)
-addo('闪灵',6)
-addo('伊芙利特',6)
-addo('塞雷娅',6)
-addo('银灰',6)
-addo('夜莺',6)
-addo('艾雅法拉',6)
-addo('安洁莉娜',6)
-addo('斯卡蒂',6)
-addo('杜宾',4)
-addo('深海色',4)
-addo('白雪',4)
-addo('芬',3)
-addo('克洛丝',3)
-addo('炎熔',3)
-# 慢慢加干员就是了'''
 
-Official6 = r"推进之王/能天使/星熊/闪灵/伊芙利特/塞雷娅/\
-银灰/夜莺/艾雅法拉/安洁莉娜/斯卡蒂"
-Official5 = r"白面鸮/幽灵鲨/芙兰卡/\
-德克萨斯/凛冬/蓝毒/普罗旺斯/临光/红/\
-赫默/雷蛇/夜魔/天火/初雪/拉普兰德/华法琳/\
-守林人/狮蝎/真理/陨星/白金/梅尔/可颂/崖心/食铁兽/空"
-Official4 = r"杜宾/深海色/白雪/远山/夜烟/流星/\
-蛇屠箱/末药/猎蜂/慕斯/砾/暗索/地灵/调香师/霜叶/\
-清道夫/角峰/古米/缠丸/阿消/红豆/杰西卡"
-Official3 = r"芬/克洛丝/炎熔/米格鲁/芙蓉/\
-卡缇/史都华德/香草/玫兰莎/安赛尔/梓兰/翎羽/空爆/月见夜"
-
-for i in Official6.split(r'/'):
-    addo(i,6)
-for i in Official5.split(r'/'):
-    addo(i,5)
-for i in Official4.split(r'/'):
-    addo(i,4)
-for i in Official3.split(r'/'):
-    addo(i,3)
-# 不行 太蠢了 程序员就得用程序员的手法
-# 字符串来自http://wiki.joyme.com/arknights/干员寻访模拟器
-
-while 1:
-    user_input = input("输入抽卡数量(或q退出),建议直接回车开启带保底的10连:") or '10'
-    if user_input == 'q':
-        break
-    try:
-        numbers = int(user_input)
-    except ValueError:
-        print("输入的不是整型,不太好抽啊")
-        continue
-    if numbers not in range(0,1001):
-        print("亲亲建议您输入0-1000的数量呢(呕)")
-        continue
-    print('\n')
-    print(draw(numbers),'\n')
+if __name__ == '__main__':
+    while 1:
+        user_input = input("输入抽卡数量(或q退出),建议直接回车开启带保底的10连:") or '10'
+        if user_input.lower() == 'q':
+            break
+        try:
+            numbers = int(user_input)
+        except ValueError:
+            print("输入的不是整型,不太好抽啊")
+            continue
+        if numbers not in range(0,1001):
+            print("亲亲建议您输入0-1000的数量呢(呕)")
+            continue
+        print('\n')
+        print(draw(numbers,STADNARD_POOL),'\n')
+    pass
 
 
 
