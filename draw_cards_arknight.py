16bb662caa5c6184b6399a6a5c3d976b194b29a8
ietar <410473517@qq.com>
2019-08-23 20:11:41 +0800

    2.3
    # v2.3 增加了Draw类 将draw()和draw10()封装其中 并增加了6星出率50抽不出后递增功能(与游戏一致)
    # 新卡池前10抽必出5星计划鸽了(这个提升也不大) 计划活动开了更新卡池

--- draw_cards_arknight.py
+++ draw_cards_arknight.py
@@ -5,6 +5,8 @@
 # v2.0 模块化 加入了标准池子STANDARD_POOL(无up, 包含限定干员) 自定义up池 池子检查等功能
 # v2.1 紧急修复了6星出率过高的bug(由10连保底机制产生)
 # v2.2 pep8 修复Official类定义中的几个漏洞
+# v2.3 增加了Draw类 将draw()和draw10()封装其中 并增加了6星出率50抽不出后递增功能(与游戏一致)
+# 新卡池前10抽必出5星计划鸽了(这个提升也不大) 计划活动开了更新卡池
 
 
 __author__ = 'ietar'
@@ -172,116 +174,161 @@ Official((str)name, (int)stars, uprate=0[, (int)otype])"""
 STANDARD_POOL = makepool()
 
 
-def draw10(pool=None):
-    if pool is None:
-        pool = STANDARD_POOL
-    return draw(10, pool)
+class Draw(object):
+    def __init__(self):
+        self.count_no_6 = 0
+        self.count_no_5 = 0
+        self.debug = False
 
+    def draw(self, times=1, pool=None):
+        if pool is None:
+            pool = STANDARD_POOL
+        pool6 = {}
+        pool5 = {}
+        pool4 = {}
+        pool3 = {}
+        temp6 = 0
+        temp5 = 0
+        rate = {6: 0.02, 5: 0.08, 4: 0.4, 3: 0.5, 2: 0, 1: 0}
+        count6, count5, count4, count3 = 0, 0, 0, 0
+        # 分别对应6, 5, 4, 3星 干员出货数量
+        ratemax6, ratemax5, ratemax4, ratemax3 = 1, 1, 1, 1
+        nonup6, nonup5, nonup4, nonup3 = 0, 0, 0, 0
+        # 分别对应6, 5, 4, 3星 非up干员数量
+        res = []
+        # pool6,5,4,3 = {干员名:同星级内抽取率}
+        # 初始化完成
 
-def draw(times=1, pool=None):
-    if pool is None:
-        pool = STANDARD_POOL
-    pool6 = {}
-    pool5 = {}
-    pool4 = {}
-    pool3 = {}
-    rate = {6: 0.02, 5: 0.08, 4: 0.4, 3: 0.5, 2: 0, 1: 0}
-    count6, count5, count4, count3 = 0, 0, 0, 0
-    # 分别对应6, 5, 4, 3星 干员出货数量
-    ratemax6, ratemax5, ratemax4, ratemax3 = 1, 1, 1, 1
-    nonup6, nonup5, nonup4, nonup3 = 0, 0, 0, 0
-    # 分别对应6, 5, 4, 3星 非up干员数量
-    res = []
-    # pool6,5,4,3 = {干员名:同星级内抽取率}
-    # 初始化完成
+        # if干员的uprate不为0 将其直接设为权重
+        # else 使其平分剩下的rate 分的人数+1
+        for i in pool:
+            if i.stars == 6:
+                pool6[i] = i.uprate
+                if i.uprate:
+                    ratemax6 -= i.uprate
+                else:
+                    nonup6 += 1
+            elif i.stars == 5:
+                pool5[i] = i.uprate
+                if i.uprate:
+                    ratemax5 -= i.uprate
+                else:
+                    nonup5 += 1
+            elif i.stars == 4:
+                pool4[i] = i.uprate
+                if i.uprate:
+                    ratemax4 -= i.uprate
+                else:
+                    nonup4 += 1
+            elif i.stars == 3:
+                pool3[i] = i.uprate
+                if i.uprate:
+                    ratemax3 -= i.uprate
+                else:
+                    nonup3 += 1
 
-    # if干员的uprate不为0 将其直接设为权重
-    # else 使其平分剩下的rate 分的人数+1
-    for i in pool:
-        if i.stars == 6:
-            pool6[i] = i.uprate
-            if i.uprate:
-                ratemax6 -= i.uprate
+        # nweight == normal weight
+        nweight6 = ratemax6 / nonup6
+        nweight5 = ratemax5 / nonup5
+        nweight4 = ratemax4 / nonup4
+        nweight3 = ratemax3 / nonup3
+
+        for i in pool6:
+            if not i.uprate:
+                pool6[i] = nweight6
+        for i in pool5:
+            if not i.uprate:
+                pool5[i] = nweight5
+        for i in pool4:
+            if not i.uprate:
+                pool4[i] = nweight4
+        for i in pool3:
+            if not i.uprate:
+                pool3[i] = nweight3
+
+        for _ in range(times):
+            a = random.random()
+            # temp6 += 1
+            # temp5 += 1
+            self.count_no_5 += 1
+            self.count_no_6 += 1
+
+            # 50后6星抽率增加
+            if self.count_no_6 > 50:
+                rate[6] = 0.02 + 0.02 * (self.count_no_6 - 50)
+                rate[5] = (1-rate[6])*0.08/0.98
+                rate[4] = (1-rate[6])*0.5/0.98
+                rate[3] = (1-rate[6])*0.4/0.98
             else:
-                nonup6 += 1
-        elif i.stars == 5:
-            pool5[i] = i.uprate
-            if i.uprate:
-                ratemax5 -= i.uprate
-            else:
-                nonup5 += 1
-        elif i.stars == 4:
-            pool4[i] = i.uprate
-            if i.uprate:
-                ratemax4 -= i.uprate
-            else:
-                nonup4 += 1
-        elif i.stars == 3:
-            pool3[i] = i.uprate
-            if i.uprate:
-                ratemax3 -= i.uprate
-            else:
-                nonup3 += 1
+                rate = {6: 0.02, 5: 0.08, 4: 0.4, 3: 0.5, 2: 0, 1: 0}
+            assert abs(1-(rate[6] + rate[5] + rate[4] + rate[3])) < 0.00001
 
-    # nweight == normal weight
-    nweight6 = ratemax6 / nonup6
-    nweight5 = ratemax5 / nonup5
-    nweight4 = ratemax4 / nonup4
-    nweight3 = ratemax3 / nonup3
+            if a <= rate[6]:
+                out = random.choices(list(pool6.keys()), list(pool6.values()))[0]
+                res.append(out)
+                count6 += 1
+                self.count_no_5 = 0
+                self.count_no_6 = 0
 
-    for i in pool6:
-        if not i.uprate:
-            pool6[i] = nweight6
-    for i in pool5:
-        if not i.uprate:
-            pool5[i] = nweight5
-    for i in pool4:
-        if not i.uprate:
-            pool4[i] = nweight4
-    for i in pool3:
-        if not i.uprate:
-            pool3[i] = nweight3
+            elif a <= rate[5]:
+                out = random.choices(list(pool5.keys()), list(pool5.values()))[0]
+                res.append(out)
+                count5 += 1
+                self.count_no_5 = 0
 
-    for _ in range(times):
-        a = random.random()
-        if a <= rate[6]:
-            out = random.choices(list(pool6.keys()), list(pool6.values()))[0]
-            res.append(out)
-            count6 += 1
-        elif a <= rate[5]:
-            out = random.choices(list(pool5.keys()), list(pool5.values()))[0]
-            res.append(out)
-            count5 += 1
-        elif a <= rate[4]:
-            out = random.choices(list(pool4.keys()), list(pool4.values()))[0]
-            res.append(out)
-            count4 += 1
+            elif a <= rate[4]:
+                out = random.choices(list(pool4.keys()), list(pool4.values()))[0]
+                res.append(out)
+                count4 += 1
+
+            else:
+                out = random.choices(list(pool3.keys()), list(pool3.values()))[0]
+                res.append(out)
+                count3 += 1
+        # 正式抽卡 完成
+
+        # 保底考虑直接重抽10次 只在10连抽时触发
+        if times == 10 and not (count6 + count5 + count4):
+            # print("保底机制使您免收紫气东来困扰 1次")
+            return self.draw(10, pool)
         else:
-            out = random.choices(list(pool3.keys()), list(pool3.values()))[0]
-            res.append(out)
-            count3 += 1
-    # 正式抽卡 完成
+            self.count_no_5 += temp5
+            self.count_no_6 += temp6
 
-    # 保底考虑直接重抽10次 只在10连抽时触发
-    if times == 10 and not (count6 + count5 + count4):
-        # print("保底机制使您免收紫气东来困扰 1次")
-        return draw(10, pool)
-    return res
+            if self.debug:
+                print('已有 {} 抽未出6星'.format(self.count_no_6))
+                print('已有 {} 抽未出5星'.format(self.count_no_5))
+                print(rate)
+            return res
+
+    def draw10(self):
+        return self.draw(times=10)
 
 
 if __name__ == '__main__':
-    while 1:
-        user_input = input("输入抽卡数量(或q退出),建议直接回车开启带保底的10连:") or '10'
-        if user_input.lower() == 'q':
-            break
-        try:
-            numbers = int(user_input)
-        except ValueError:
-            print("输入的不是整型,不太好抽啊")
-            continue
-        if numbers not in range(0, 1001):
-            print("亲亲建议您输入0-1000的数量呢(呕)")
-            continue
-        print('\n')
-        print(draw(numbers, STANDARD_POOL), '\n')
-    pass
+    d = Draw()
+    mode = input('输入d进入调试模式 输入其他进入用户友好模式:')
+    if mode.lower() != 'd':
+        while 1:
+            user_input = input("输入抽卡数量(或q退出),建议直接回车开启带保底的10连:") or '10'
+            if user_input.lower() == 'q':
+                break
+            try:
+                numbers = int(user_input)
+            except ValueError:
+                print("输入的不是整型,不太好抽啊")
+                continue
+            if numbers not in range(0, 1001):
+                print("亲亲建议您输入0-1000的数量呢(呕)")
+                continue
+            print('\n')
+            print(d.draw(numbers, STANDARD_POOL), '\n')
+    else:
+        d.debug = True
+        print(d.draw())
+        print(d.draw10())
+        while 1:
+            confirm = input('输入q退出 输入其他继续10连:')
+            if confirm.lower() == 'q':
+                break
+            print(d.draw10())
